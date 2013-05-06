a2e833675fbd3f958e993ce9524aab4fda5005d0
phisikus <phisikus@platinum.linux.pl>
2013-05-06 23:44:15 +0200

    Delete logs

--- application/logs/2012/08/09.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2012-08-09 15:13:01 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL show/admin was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 113 ]
-2012-08-09 15:13:01 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL show/admin was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 113 ]
---
-#0 /var/www/magog/system/classes/kohana/request/client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
-#1 /var/www/magog/system/classes/kohana/request.php(1138): Kohana_Request_Client->execute(Object(Request))
-#2 /var/www/magog/index.php(109): Kohana_Request->execute()
-#3 {main}
