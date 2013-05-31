530baf4e5a2672f3bab0e96d3a6d65103a48540e
phisikus <phisikus@platinum.linux.pl>
2013-05-31 15:40:26 +0200

    Almost working backend for jquery menu

--- application/classes/controller/menu/admin.php
+++ application/classes/controller/menu/admin.php
@@ -25,112 +25,113 @@
 
 defined('SYSPATH') or die('No direct script access.');
 require("application/classes/controller/main.php");
- 
- class Controller_Menu_Admin extends Controller_Main {
-	 
-	 function prepare_menu(&$menu)
-	 {
-		 $menu[0][0] =  __('Panel');
-		 $menu[0][1] =  '';
-		 $menu[0][2] =  __('Profil');
-		 $menu[0][3] = 'user/user/index';
-		 $menu[0][4] =  __('Wyloguj');
-		 $menu[0][5] =  'admin/logout';
-	 }
-	 
-	 public function before()
-	 {
-		 parent::before();
-		 if(!(Auth::instance()->logged_in('admin')))
-			$this->request->redirect('index.php/admin/login');
-	 }
-	 
-	 public function action_index()
-	 {
-		 $content = __("<h1>Konfiguracja menu</h1>");
-		
-		 $level[0]['description'] = __("Zarządzanie menu");
-		 $level[0]['path'] ='menu/admin';	
-		  
-		 $this->prepare_menu($menu);
 
-		 $mmenu = new Model_Menu;
-		 $menus = $mmenu->getAllMenus();
-		 $am = $mmenu->getAllMenuEntrys();
-		 
-		 $mpage = new Model_Page;
-		 $ap = $mpage->getAllArticlesId();	 
-		 
-		 
-		 if(is_numeric($this->request->param('id')))
-		 {
-				 $entry = $mmenu->getMenuEntry($this->request->param('id'));
-				 
-				 $content = $content.View::factory('menu/admin')
-							->set('menus',$menus)
-							->set('all_menus',$am)
-							->set('all_pages',$ap)
-							->set('entry',$entry)			
-							->render();
-		 }
-		 else
-				$content = $content.View::factory('menu/admin')
-					->set('menus',$menus)
-					->set('all_menus',$am)			
-					->set('all_pages',$ap)
-					->render();
+class Controller_Menu_Admin extends Controller_Main
+{
 
-			$view = View::factory('admin/panel');
-			$view->set('content',$content);
-			$view->set('level',$level);
-			$view->set('menu',$menu);
-		 $this->response->body($view->render()); 
-		
-	 }
-	 
-	 public function action_order()
-	 {
-         $this->response->headers('Content-Type','application/json');
-         Log::instance()->add(Log::NOTICE, $this->req);
-         $this->response->body($this->request->body());
+    function prepare_menu(&$menu)
+    {
+        $menu[0][0] = __('Panel');
+        $menu[0][1] = '';
+        $menu[0][2] = __('Profil');
+        $menu[0][3] = 'user/user/index';
+        $menu[0][4] = __('Wyloguj');
+        $menu[0][5] = 'admin/logout';
+    }
 
-		 //$this->request->redirect('index.php/menu/admin');
-	 }
-	 
-	 public function action_delete()
-	 {
-		 $mmenu = new Model_Menu;
-		 $mmenu->deleteMenuOrEntry($this->request->param('id'));
-		 $this->request->redirect('index.php/menu/admin');
-	 }
-	 
-	 public function action_save()
-	 {
-		 $mmenu = new Model_Menu;
-		 $id = $this->request->param('id');
-		 
-		 if(Arr::get($_POST,'page_selected') != '*')
-		 {
-			 if(Arr::get($_POST,'page_selected') == 0)
-				$url = url::site('show/news').'/1=0=0';
-			else
-				$url = url::site('show/page').'/'.HTML::chars(Arr::get($_POST,'page_selected'));
-		 }
-		else
-			$url = HTML::chars(Arr::get($_POST,'link'));
-			
-		 		 
-		 $mmenu->saveMenuEntry($id,HTML::chars(Arr::get($_POST,'text')),
-									$url,
-									HTML::chars(Arr::get($_POST,'parent_id')),
-									1,1);
-	 	 $this->request->redirect('index.php/menu/admin');
-		 
-	 }
-	 
+    public function before()
+    {
+        parent::before();
+        if (!(Auth::instance()->logged_in('admin')))
+            $this->request->redirect('index.php/admin/login');
+    }
 
- }
-	
- 
+    public function action_index()
+    {
+        $content = __("<h1>Konfiguracja menu</h1>");
+
+        $level[0]['description'] = __("Zarządzanie menu");
+        $level[0]['path'] = 'menu/admin';
+
+        $this->prepare_menu($menu);
+
+        $mmenu = new Model_Menu;
+        $menus = $mmenu->getAllMenus();
+        $am = $mmenu->getAllMenuEntrys();
+
+        $mpage = new Model_Page;
+        $ap = $mpage->getAllArticlesId();
+
+
+        if (is_numeric($this->request->param('id'))) {
+            $entry = $mmenu->getMenuEntry($this->request->param('id'));
+
+            $content = $content . View::factory('menu/admin')
+                ->set('menus', $menus)
+                ->set('all_menus', $am)
+                ->set('all_pages', $ap)
+                ->set('entry', $entry)
+                ->render();
+        } else
+            $content = $content . View::factory('menu/admin')
+                ->set('menus', $menus)
+                ->set('all_menus', $am)
+                ->set('all_pages', $ap)
+                ->render();
+
+        $view = View::factory('admin/panel');
+        $view->set('content', $content);
+        $view->set('level', $level);
+        $view->set('menu', $menu);
+        $this->response->body($view->render());
+
+    }
+
+    public function action_order()
+    {
+        $this->response->headers('Content-Type', 'application/json');
+        $menus = json_decode($this->request->body());
+
+        $mmenu = new Model_Menu();
+        $mmenu->updateMenuStructure($menus,null);
+
+        $response["status"] = "ok";
+        $this->response->body(json_encode($response));
+
+    }
+
+    public function action_delete()
+    {
+        $mmenu = new Model_Menu;
+        $mmenu->deleteMenuOrEntry($this->request->param('id'));
+        $this->request->redirect('index.php/menu/admin');
+    }
+
+    public function action_save()
+    {
+        $mmenu = new Model_Menu;
+        $id = $this->request->param('id');
+
+        if (Arr::get($_POST, 'page_selected') != '*') {
+            if (Arr::get($_POST, 'page_selected') == 0)
+                $url = url::site('show/news') . '/1=0=0';
+            else
+                $url = url::site('show/page') . '/' . HTML::chars(Arr::get($_POST, 'page_selected'));
+        } else
+            $url = HTML::chars(Arr::get($_POST, 'link'));
+
+
+        $mmenu->saveMenuEntry($id, HTML::chars(Arr::get($_POST, 'text')),
+            $url,
+            HTML::chars(Arr::get($_POST, 'parent_id')),
+            1, 1);
+        $this->request->redirect('index.php/menu/admin');
+
+    }
+
+
+}
+
+
 ?>
 
--- application/classes/model/menu.php
+++ application/classes/model/menu.php
@@ -23,201 +23,178 @@
  * 
  */
 
- defined('SYSPATH') or die('No direct script access.');
+defined('SYSPATH') or die('No direct script access.');
 
-class Model_Menu extends ORM {
+class Model_Menu extends ORM
+{
 
-	 protected $_table_columns = array(
+    protected $_table_columns = array(
         'id' => array('type' => 'int', 'is_nullable' => false),
         'text' => array('type' => 'string', 'is_nullable' => true),
-		'parent_id' => array('type' => 'int', 'is_nullable' => true),
-		'link' => array('type' => 'string', 'is_nullable' => true),
-		'enabled' => array('type' => 'int', 'is_nullable' => false),
-		'visible' => array('type' => 'int', 'is_nullable' => false)
+        'parent_id' => array('type' => 'int', 'is_nullable' => true),
+        'link' => array('type' => 'string', 'is_nullable' => true),
+        'enabled' => array('type' => 'int', 'is_nullable' => false),
+        'visible' => array('type' => 'int', 'is_nullable' => false)
     );
 
-   public $_table_name = 'menu';
-   public $_has_many = array('entrys' => array('model' => 'menu','foreign_key' => 'parent_id'));
+    public $_table_name = 'menu';
+    public $_has_many = array('entrys' => array('model' => 'menu', 'foreign_key' => 'parent_id'));
 
-   public function createMenu($name)
-   {
-	   $menu = ORM::factory('menu');
-	   $menu->text = $name;
-	   $menu->link = '';
-	   $menu->enabled = 1;
-	   $menu->visible = 1;
-	   $menu->save();
+    public function createMenu($name)
+    {
+        $menu = ORM::factory('menu');
+        $menu->text = $name;
+        $menu->link = '';
+        $menu->enabled = 1;
+        $menu->visible = 1;
+        $menu->save();
 
-	   return $menu->id;
-   }
+        return $menu->id;
+    }
 
-   public function deleteMenuOrEntry($id)
-   {
-	   $menu = ORM::factory('menu')
-			->where('id','=',$id)
-			->find();
+    public function deleteMenuOrEntry($id)
+    {
+        $menu = ORM::factory('menu')
+            ->where('id', '=', $id)
+            ->find();
 
-	   $menu->delete();
-   }
+        $menu->delete();
+    }
 
-   public function addMenuEntry($id,$text,$link)
-   {
-	   $menu_parent = ORM::factory('menu')->where('id','=',$id)->find();
+    public function addMenuEntry($id, $text, $link)
+    {
+        $menu_parent = ORM::factory('menu')->where('id', '=', $id)->find();
 
-	   $menu = ORM::factory('menu');
-	   $menu->text = $text;
-	   $menu->parent_id = $id;
-	   $menu->link = $link;
-	   $menu->enabled = 1;
-	   $menu->visible = 1;
-	   $menu->save();
+        $menu = ORM::factory('menu');
+        $menu->text = $text;
+        $menu->parent_id = $id;
+        $menu->link = $link;
+        $menu->enabled = 1;
+        $menu->visible = 1;
+        $menu->save();
 
-	   return $menu->id;
-   }
+        return $menu->id;
+    }
 
-   public function saveMenuEntry($id,$text,$link,$parent_id,$enabled,$visible)
-   {
-	   if($id == 0)
-			$menu = ORM::factory('menu');
-		else
-			$menu = ORM::factory('menu')->where('id','=',$id)->find();
+    public function saveMenuEntry($id, $text, $link, $parent_id, $enabled, $visible)
+    {
+        if ($id == 0)
+            $menu = ORM::factory('menu');
+        else
+            $menu = ORM::factory('menu')->where('id', '=', $id)->find();
 
-	   $menu->text = $text;
+        $menu->text = $text;
 
-	   if(!empty($parent_id))
-		$menu->parent_id = $parent_id;
+        if (!empty($parent_id))
+            $menu->parent_id = $parent_id;
 
-	   $menu->link = $link;
-	   $menu->enabled = $enabled;
-	   $menu->visible = $visible;
-	   $menu->save();
+        $menu->link = $link;
+        $menu->enabled = $enabled;
+        $menu->visible = $visible;
+        $menu->save();
 
-   }
+    }
 
-   public function setEnablance($id,$val)
-   {
-	   	   $menu = ORM::factory('menu')->where('id','=',$id)->find();
-	   	   $menu->enable = $val;
-	   	   $menu->save();
-   }
+    public function setEnablance($id, $val)
+    {
+        $menu = ORM::factory('menu')->where('id', '=', $id)->find();
+        $menu->enable = $val;
+        $menu->save();
+    }
 
-   public function setVisibility($id,$val)
-   {
-	   	   $menu = ORM::factory('menu')->where('id','=',$id)->find();
-	   	   $menu->visible = $val;
-	   	   $menu->save();
-   }
-
-
-   public function hasSubmenu($id)
-   {
-	   return (ORM::factory('menu')
-				->where('parent_id','=',$id)
-				->count_all() > 0 ? true : false);
-   }
-
-   public function getMenuEntry($id)
-   {
-	   return ORM::factory('menu')->where('id','=',$id)->find();
-
-   }
-
-   public function getMenuByName($name)
-   {
-	   return ORM::factory('menu')->where('text','=',$name)->find();
-
-   }
+    public function setVisibility($id, $val)
+    {
+        $menu = ORM::factory('menu')->where('id', '=', $id)->find();
+        $menu->visible = $val;
+        $menu->save();
+    }
 
 
+    public function hasSubmenu($id)
+    {
+        return (ORM::factory('menu')
+            ->where('parent_id', '=', $id)
+            ->count_all() > 0 ? true : false);
+    }
 
-   public function getMenuList()
-   {
-		return ORM::factory('menu')->where('parent_id','=', NULL)->find_all();
-   }
+    public function getMenuEntry($id)
+    {
+        return ORM::factory('menu')->where('id', '=', $id)->find();
 
-   public function getMenu($id,&$no,$level,&$tab)
-   {
-	   $menu = ORM::factory('menu')->where('id','=',$id)->find();
-	   $submenu = $menu->entrys->order_by('id','asc')->find_all();
+    }
 
-	   $tab[$no] = array($menu,$level);
+    public function getMenuByName($name)
+    {
+        return ORM::factory('menu')->where('text', '=', $name)->find();
 
-	      foreach($submenu as $sm)
-		   {
-			   $this->getMenu($sm->id,++$no,$level+1,$tab);
-		   }
-   }
+    }
 
-   public function getAllMenus()
-   {
-	   $res = ORM::factory('menu')->where('parent_id','=', NULL)->find_all();
-	   $i = 0;
-	   $result = '';
-	   foreach($res as $re)
-	   {
-		   $j = 0;
-			$this->getMenu($re->id,$j,0,$result[$i++]);
-	   }
 
-	   return $result;
-   }
+    public function getMenuList()
+    {
+        return ORM::factory('menu')->where('parent_id', '=', NULL)->find_all();
+    }
 
-   public function getAllMenuEntrys()
-   {
-	  return ORM::factory('menu')->find_all();
-   }
+    public function getMenu($id, &$no, $level, &$tab)
+    {
+        $menu = ORM::factory('menu')->where('id', '=', $id)->find();
+        $submenu = $menu->entrys->order_by('id', 'asc')->find_all();
 
-   public function swapMenuEntry($id1,$id2)
-   {
-	      $menu1 = ORM::factory('menu')->where('id','=',$id1)->find();
-	      $menu2 = ORM::factory('menu')->where('id','=',$id2)->find();
+        $tab[$no] = array($menu, $level);
 
-	      // Swap two elements position
-	      list($menu1->link, $menu2->link) = array($menu2->link, $menu1->link);
-	      list($menu1->text, $menu2->text) = array($menu2->text, $menu1->text);
-	      list($menu1->enabled, $menu2->enabled) = array($menu2->enabled, $menu1->enabled);
-	      list($menu1->visible, $menu2->visible) = array($menu2->visible, $menu1->visible);
+        foreach ($submenu as $sm) {
+            $this->getMenu($sm->id, ++$no, $level + 1, $tab);
+        }
+    }
 
-	      $menu1->save();
-	      $menu2->save();
-   }
+    public function getAllMenus()
+    {
+        $res = ORM::factory('menu')->where('parent_id', '=', NULL)->find_all();
+        $i = 0;
+        $result = '';
+        foreach ($res as $re) {
+            $j = 0;
+            $this->getMenu($re->id, $j, 0, $result[$i++]);
+        }
 
-   public function setHigher($id)
-   {
-	   $menu_higher = ORM::factory('menu')->where('id','<',$id)->order_by('id','desc')->find();
-	   if(!empty($menu_higher->id))
-			$this->swapMenuEntry($id,$menu_higher->id);
-   }
+        return $result;
+    }
 
-   public function setLower($id)
-   {
-	   $menu_lower = ORM::factory('menu')->where('id','>',$id)->order_by('id','asc')->find();
-	   if(!empty($menu_lower->id))
-			$this->swapMenuEntry($id,$menu_lower->id);
-   }
+    public function getAllMenuEntrys()
+    {
+        return ORM::factory('menu')->find_all();
+    }
 
-   public function setRight($id)
-   {
-	   $menu_right = ORM::factory('menu')->where('id','<',$id)->order_by('id','desc')->find();
-	   if(!empty($menu_right->id))
-	   {
-		   $menu = ORM::factory('menu')->where('id','=',$id)->find();
-		   $menu->parent_id = $menu_right->id;
-		   $menu->save();
-	   }
-   }
 
-   public function setLeft($id)
-   {
-	   $menu = ORM::factory('menu')->where('id','=',$id)->find();
-	   $menu_left = ORM::factory('menu')->where('id','=',$menu->parent_id)->order_by('id','desc')->find();
-	   if(!empty($menu_left->id))
-	   {
-		   $menu->parent_id = $menu_left->parent_id;
-		   $menu->save();
-	   }
-   }
+    public function updateMenuStructure($list, $parentId)
+    {
+        foreach ($list as $element) {
+            $mElement = $this->getMenuEntry($element->id);
+            $mElement->parent_id = $parentId;
+            $mElement->save();
+            if (isset($element->children)) {
+                if (!$this->updateMenuStructure($element->children, $element->id))
+                    return false;
+            }
 
+        }
+        return true;
+    }
+
+    public function swapMenuEntry($id1, $id2)
+    {
+        $menu1 = ORM::factory('menu')->where('id', '=', $id1)->find();
+        $menu2 = ORM::factory('menu')->where('id', '=', $id2)->find();
+
+        // Swap two elements position
+        list($menu1->link, $menu2->link) = array($menu2->link, $menu1->link);
+        list($menu1->text, $menu2->text) = array($menu2->text, $menu1->text);
+        list($menu1->enabled, $menu2->enabled) = array($menu2->enabled, $menu1->enabled);
+        list($menu1->visible, $menu2->visible) = array($menu2->visible, $menu1->visible);
+
+        $menu1->save();
+        $menu2->save();
+    }
 
 
 }
